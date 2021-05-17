ee5a9f0f58d249699e8c6b99f0ca4185a590a9b2
Nataliia Koval <hakunata@gmail.com>
2021-05-17 23:29:44 +0300

    try to create a chat

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import io from 'socket.io-client';
 import './App.css';
 
+let socket;
+const CONNECTION_PORT = 'localhost:3002/';
+
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [userName, setUserName] = useState('');
+  const [room, setRoom] = useState('');
+
+  const [message, setMessage] = useState('');
+  const [messageList, setMessageList] = useState([]);
+
+  useEffect(() => {
+    socket = io(CONNECTION_PORT);
+  }, []);
+
+  useEffect(() => {
+    socket.on('receive_message', (data) => {
+      setMessageList([...messageList, data]);
+    })
+  })
+
+  const connectToRoom = () => {
+    setLoggedIn(true);
+    socket.emit('join_room', room);
+  }
+
+  const sendMessage = async () => {
+    let messageContent = {
+      room: room,
+      content: {
+        author: userName,
+        message: message
+      }
+    };
+    await socket.emit('send_message', messageContent);
+    setMessageList([...messageList, messageContent.content]);
+    setMessage('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        !loggedIn ? (
+            <div className="logIn">
+              <div className="inputs">
+                <input type="text" placeholder="Name" onChange={(e) => {setUserName(e.target.value)}}/>
+                <input type="text" placeholder="Room" onChange={(e) => {setRoom(e.target.value)}}/>
+              </div>
+              <button onClick={connectToRoom}>Enter Chat</button>
+            </div>
+        ) : (
+            <div className="chatContainer">
+              <div className="messages">
+                {messageList.map((val, key) => {
+                  return (
+                    <div key={key} className="messageItem" id={val.author === userName ? "you" : "other"}>
+                      <div className="author">{val.author}</div>
+                      <div>{val.message}</div>
+                    </div>
+                  )
+                })}
+              </div>
+
+              <div className="messageInputs">
+                <input type="text" placeholder="Message" onChange={(e) => {setMessage(e.target.value)}}/>
+                <button onClick={sendMessage}>Send</button>
+              </div>
+            </div>
+        )
+      }
     </div>
   );
 }
